1e06b2efb616bf812b6e8f22aeab4e29de7bbfe3
Frederic Weisbecker <fweisbec@gmail.com>
2014-07-17 15:56:40 +0200

    nohz: Kick full dynticks timer targets with an empty IPI
    While we enqueue a new timer on a dynticks target, we must send it an
    IPI so that it reschedules the next tick accordingly.
    
    Now all we need for that is to run irq_exit() on the target. Hence
    an empty IRQ is way enough. We don't need to run tick_nohz_full_check()
    and all the overhead that comes with the several cmpxchg() necessary for
    irq work list and state progress.
    
    So lets use a void irq work instead.
    
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -693,7 +693,7 @@ static bool wake_up_full_nohz_cpu(int cpu)
 	if (tick_nohz_full_cpu(cpu)) {
 		if (cpu != smp_processor_id() ||
 		    tick_nohz_tick_stopped())
-			tick_nohz_full_kick_cpu(cpu);
+			irq_work_void_on(cpu);
 		return true;
 	}
 
