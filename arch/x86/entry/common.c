330428ecf28ab50fe5763d3362b9f8d57e86eddc
Frederic Weisbecker <fweisbec@gmail.com>
2017-09-10 18:52:57 +0200

    x86: Use lockdep to assert IRQs are disabled/enabled
    Use lockdep to check that IRQs are enabled or disabled as expected. This
    way the sanity check only shows overhead when concurrency correctness
    debug code is enabled.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- arch/x86/entry/common.c
+++ arch/x86/entry/common.c
@@ -186,7 +186,7 @@ __visible inline void prepare_exit_to_usermode(struct pt_regs *regs)
 
 	addr_limit_user_check();
 
-	if (IS_ENABLED(CONFIG_PROVE_LOCKING) && WARN_ON(!irqs_disabled()))
+	if (!lockdep_assert_irqs_disabled())
 		local_irq_disable();
 
 	lockdep_sys_exit();
