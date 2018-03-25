b6e4a12e8c27a20ab89e20fbf5ea8acc0061918a
Frederic Weisbecker <frederic@kernel.org>
2018-03-25 16:01:06 +0200

    sparc: Convert local_softirq_pending() to use per-cpu op
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>

--- arch/sparc/include/asm/hardirq_64.h
+++ arch/sparc/include/asm/hardirq_64.h
@@ -11,7 +11,7 @@
 
 #define __ARCH_IRQ_STAT
 #define local_softirq_pending() \
-	(local_cpu_data().__softirq_pending)
+	(__this_cpu_read(__cpu_data.__softirq_pending))
 
 void ack_bad_irq(unsigned int irq);
 
