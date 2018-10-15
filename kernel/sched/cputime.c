0e6c849e722b0179f89027394525b40d11cd9d61
Frederic Weisbecker <frederic@kernel.org>
2018-10-15 03:50:32 +0200

    sched/vtime: Record CPU under seqcount for kcpustat needs
    In order to compute the kcpustat delta on a nohz CPU, we'll need to
    fetch the task running on that target. Checking that its vtime
    state snapshot actually refers to the relevant target involves recording
    that CPU under the seqcount locked on task switch.
    
    This is a step toward making kcpustat moving forward on full nohz CPUs.
    
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -817,6 +817,7 @@ void vtime_task_switch_generic(struct task_struct *prev)
 	else
 		__vtime_account_kernel(prev, vtime);
 	vtime->state = VTIME_INACTIVE;
+	vtime->cpu = -1;
 	write_seqcount_end(&vtime->seqcount);
 
 	vtime = &current->vtime;
@@ -824,6 +825,7 @@ void vtime_task_switch_generic(struct task_struct *prev)
 	write_seqcount_begin(&vtime->seqcount);
 	vtime->state = VTIME_SYS;
 	vtime->starttime = sched_clock();
+	vtime->cpu = smp_processor_id();
 	write_seqcount_end(&vtime->seqcount);
 }
 
@@ -836,6 +838,7 @@ void vtime_init_idle(struct task_struct *t, int cpu)
 	write_seqcount_begin(&vtime->seqcount);
 	vtime->state = VTIME_SYS;
 	vtime->starttime = sched_clock();
+	vtime->cpu = cpu;
 	write_seqcount_end(&vtime->seqcount);
 	local_irq_restore(flags);
 }
