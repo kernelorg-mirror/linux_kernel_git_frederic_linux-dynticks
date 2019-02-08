0b7311c2a7a368605b35e65aec816e2c09b03540
Frederic Weisbecker <frederic@kernel.org>
2019-02-09 00:17:58 +0100

    softirq: Uninline !CONFIG_TRACE_IRQFLAGS __local_bh_disable_ip()
    The common code between both versions of __local_bh_disable_ip(), whether
    CONFIG_TRACE_IRQFLAGS is on or off, is going to grow up in order to
    support vector masking granularity.
    
    Merge these versions together to prepare for that.
    
    Reviewed-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>
    Cc: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
    Cc: Joel Fernandes <joel@joelfernandes.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Pavan Kondeti <pkondeti@codeaurora.org>
    Cc: Paul E . McKenney <paulmck@linux.vnet.ibm.com>
    Cc: David S . Miller <davem@davemloft.net>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>

--- include/linux/bottom_half.h
+++ include/linux/bottom_half.h
@@ -28,17 +28,7 @@ enum
 
 #define SOFTIRQ_DATA_INIT (SOFTIRQ_ALL_MASK << SOFTIRQ_ENABLED_SHIFT)
 
-
-
-#ifdef CONFIG_TRACE_IRQFLAGS
 extern void __local_bh_disable_ip(unsigned long ip, unsigned int cnt);
-#else
-static __always_inline void __local_bh_disable_ip(unsigned long ip, unsigned int cnt)
-{
-	preempt_count_add(cnt);
-	barrier();
-}
-#endif
 
 static inline void local_bh_disable(void)
 {
