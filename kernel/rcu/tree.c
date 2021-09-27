306badf8ea025ef182a42acb71e8535bc2c493a3
Frederic Weisbecker <frederic@kernel.org>
2021-09-27 14:56:32 +0200

    rcu/nocb: Use appropriate rcu_nocb_lock_irqsave()
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -2476,8 +2476,7 @@ static void rcu_do_batch(struct rcu_data *rdp)
 	 * races with call_rcu() from interrupt handlers.  Leave the
 	 * callback counts, as rcu_barrier() needs to be conservative.
 	 */
-	local_irq_save(flags);
-	rcu_nocb_lock(rdp);
+	rcu_nocb_lock_irqsave(rdp, flags);
 	WARN_ON_ONCE(cpu_is_offline(smp_processor_id()));
 	pending = rcu_segcblist_n_cbs(&rdp->cblist);
 	div = READ_ONCE(rcu_divisor);
@@ -2540,8 +2539,7 @@ static void rcu_do_batch(struct rcu_data *rdp)
 		}
 	}
 
-	local_irq_save(flags);
-	rcu_nocb_lock(rdp);
+	rcu_nocb_lock_irqsave(rdp, flags);
 	rdp->n_cbs_invoked += count;
 	trace_rcu_batch_end(rcu_state.name, count, !!rcl.head, need_resched(),
 			    is_idle_task(current), rcu_is_callbacks_kthread());
