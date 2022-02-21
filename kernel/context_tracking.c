e722e8b51e58146ade67ae32eca4456e14c4be0f
Frederic Weisbecker <frederic@kernel.org>
2022-02-21 23:26:27 +0100

    context_tracking: Rename context_tracking_cpu_set() to context_tracking_cpu_track_user()
    context_tracking_cpu_set() is called in order to tell a CPU to track
    user/kernel transitions. Since context tracking is going to expand in
    to also track transitions from/to idle/IRQ/NMIs, the scope
    of this function name becomes too broad and needs to be made more
    specific.
    
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>
    Cc: Paul E. McKenney <paulmck@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Neeraj Upadhyay <quic_neeraju@quicinc.com>
    Cc: Uladzislau Rezki <uladzislau.rezki@sony.com>
    Cc: Joel Fernandes <joel@joelfernandes.org>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: Nicolas Saenz Julienne <nsaenz@kernel.org>
    Cc: Marcelo Tosatti <mtosatti@redhat.com>
    Cc: Xiongfeng Wang <wangxiongfeng2@huawei.com>
    Cc: Yu Liao<liaoyu15@huawei.com>
    Cc: Phil Auld <pauld@redhat.com>
    Cc: Paul Gortmaker<paul.gortmaker@windriver.com>
    Cc: Alex Belits <abelits@marvell.com>

--- kernel/context_tracking.c
+++ kernel/context_tracking.c
@@ -198,7 +198,7 @@ void user_exit_callable(void)
 }
 NOKPROBE_SYMBOL(user_exit_callable);
 
-void __init context_tracking_cpu_set(int cpu)
+void __init context_tracking_cpu_track_user(int cpu)
 {
 	static __initdata bool initialized = false;
 
@@ -228,6 +228,6 @@ void __init context_tracking_init(void)
 	int cpu;
 
 	for_each_possible_cpu(cpu)
-		context_tracking_cpu_set(cpu);
+		context_tracking_cpu_track_user(cpu);
 }
 #endif
