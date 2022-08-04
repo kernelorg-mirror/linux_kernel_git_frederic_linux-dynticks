fa80d23b2c558de5fb4d1a5ed4128cf8ae2cfcfd
Frederic Weisbecker <frederic@kernel.org>
2022-08-04 14:18:43 +0200

    sched: Remove RCU protection from get_nohz_timer_target()
    In order for housekeeping cpumask changes to avoid races against
    migrating timers, fetching the housekeeping cpumask and the actual
    timer enqueue must remain in the same RCU read side critical section.
    
    As a matter of fact, get_nohz_timer_target() is called with IRQs disabled
    until the target timer is enqueued. Therefore rcu_read_lock() is
    unecessary.
    
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1079,6 +1079,8 @@ int get_nohz_timer_target(void)
 	struct sched_domain *sd;
 	const struct cpumask *hk_mask;
 
+	lockdep_assert_irqs_enabled();
+
 	if (housekeeping_cpu(cpu, HK_TYPE_NOHZ_FULL)) {
 		if (!idle_cpu(cpu))
 			return cpu;
@@ -1086,26 +1088,20 @@ int get_nohz_timer_target(void)
 	}
 
 	hk_mask = housekeeping_cpumask(HK_TYPE_NOHZ_FULL);
-
-	rcu_read_lock();
 	for_each_domain(cpu, sd) {
 		for_each_cpu_and(i, sched_domain_span(sd), hk_mask) {
 			if (cpu == i)
 				continue;
 
-			if (!idle_cpu(i)) {
-				cpu = i;
-				goto unlock;
-			}
+			if (!idle_cpu(i))
+				return i;
 		}
 	}
 
 	if (default_cpu == -1)
 		default_cpu = housekeeping_any_cpu(HK_TYPE_NOHZ_FULL);
-	cpu = default_cpu;
-unlock:
-	rcu_read_unlock();
-	return cpu;
+
+	return default_cpu;
 }
 
 /*
