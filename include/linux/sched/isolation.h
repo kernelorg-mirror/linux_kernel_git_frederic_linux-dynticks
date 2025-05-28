1b3ef34724f07e4bf85d96784c0e6949bccefb02
Frederic Weisbecker <frederic@kernel.org>
2025-05-28 18:21:32 +0200

    cpuset: Remove cpuset_cpu_is_isolated()
    The set of cpuset isolated CPUs is now included in HK_TYPE_DOMAIN
    housekeeping cpumask. There is no usecase left interested in just
    checking what is isolated by cpuset and not by the isolcpus= kernel
    boot parameter.
    
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>
    Cc: "Michal Koutný" <mkoutny@suse.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Marco Crivellari <marco.crivellari@suse.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Waiman Long <longman@redhat.com>
    Cc: cgroups@vger.kernel.org

--- include/linux/sched/isolation.h
+++ include/linux/sched/isolation.h
@@ -83,8 +83,7 @@ static inline void housekeeping_init(void) { }
 static inline bool cpu_is_isolated(int cpu)
 {
 	return !housekeeping_test_cpu(cpu, HK_TYPE_DOMAIN) ||
-	       !housekeeping_test_cpu(cpu, HK_TYPE_TICK) ||
-	       cpuset_cpu_is_isolated(cpu);
+	       !housekeeping_test_cpu(cpu, HK_TYPE_TICK);
 }
 
 #endif /* _LINUX_SCHED_ISOLATION_H */
