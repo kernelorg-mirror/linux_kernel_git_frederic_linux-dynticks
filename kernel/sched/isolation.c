addba52e69588216450ea5fcb5b67c299a4f55ec
Frederic Weisbecker <frederic@kernel.org>
2025-06-04 14:02:40 +0200

    kthread: Honour kthreads preferred affinity after cpuset changes
    When cpuset isolated partitions get updated, unbound kthreads get
    indifferently affine to all non isolated CPUs, regardless of their
    individual affinity preferences.
    
    For example kswapd is a per-node kthread that prefers to be affine to
    the node it refers to. Whenever an isolated partition is created,
    updated or deleted, kswapd's node affinity is going to be broken if any
    CPU in the related node is not isolated because kswapd will be affine
    globally.
    
    Fix this with letting the consolidated kthread managed affinity code do
    the affinity update on behalf of cpuset.
    
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>
    Cc: Michal Koutný <mkoutny@suse.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Marco Crivellari <marco.crivellari@suse.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Waiman Long <longman@redhat.com>
    Cc: cgroups@vger.kernel.org

--- kernel/sched/isolation.c
+++ kernel/sched/isolation.c
@@ -157,6 +157,9 @@ int housekeeping_update(struct cpumask *isol_mask, enum hk_type type)
 	err = tmigr_isolated_exclude_cpumask(isol_mask);
 	WARN_ON_ONCE(err < 0);
 
+	err = kthreads_update_housekeeping();
+	WARN_ON_ONCE(err < 0);
+
 	kfree(old);
 
 	return err;
