6dddcb6399ff185b4d7fd5adfc0768ca10e83b75
Frederic Weisbecker <frederic@kernel.org>
2025-12-22 00:55:49 +0100

    cpuset: Propagate cpuset isolation update to timers through housekeeping
    Until now, cpuset would propagate isolated partition changes to
    timer migration so that unbound timers don't get migrated to isolated
    CPUs.
    
    Since housekeeping now centralizes, synchronize and propagates isolation
    cpumask changes, perform the work from that subsystem for consolidation
    and consistency purposes.
    
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>

--- kernel/sched/isolation.c
+++ kernel/sched/isolation.c
@@ -150,7 +150,12 @@ int housekeeping_update(struct cpumask *isol_mask, enum hk_type type)
 	pci_probe_flush_workqueue();
 	mem_cgroup_flush_workqueue();
 	vmstat_flush_workqueue();
+
 	err = workqueue_unbound_housekeeping_update(housekeeping_cpumask(type));
+	WARN_ON_ONCE(err < 0);
+
+	err = tmigr_isolated_exclude_cpumask(isol_mask);
+	WARN_ON_ONCE(err < 0);
 
 	kfree(old);
 
